9a63133050157a490fdb0b061f1a6e3f405e7ae2
thang <thanglhfx12120@funix.edu.vn>
2022-06-17 16:38:10 +0700

    1

--- src/store.js
+++ src/store.js
@@ -1,12 +1,12 @@
 import {createStore, applyMiddleware} from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
-import postsReducer from './features/posts/postsSlice'
+import rootReducer from './reducer'
 
 const composedEnhancer = composeWithDevTools(
     applyMiddleware(thunkMiddleware)
 )
 
-const store = createStore(postsReducer, composedEnhancer)
+const store = createStore(rootReducer, composedEnhancer)
 
 export default store
